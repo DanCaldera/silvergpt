a5f81fffa16fcc07a275ec1a7138af98f57a449e
Daniel Caldera <bdcaldera@gmail.com>
2023-05-11 01:47:38 -0600

    Added auth 0 base login and protected routes

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,3 +1,11 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+
 export default function TokenTopUpPage() {
   return <div>TokenTopUpPage</div>
 }
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {}
+  }
+})
